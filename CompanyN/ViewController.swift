4030dd72c0bcce03c1e0896de85b8a9f95495cea
docavd <docavd@yandex.ru>
2021-02-10 20:28:55 +0300

    Initial Commit

---
+++ CompanyN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompanyN
+//
+//  Created by 111 on 2/10/21.
+//  Copyright © 2021 111. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
